207ad4bbbebfba60cf783d719446294961e25c95
dragekk <53555592+dragekk@users.noreply.github.com>
2023-01-13 17:30:47 +0100

    fixed fix for math

--- grid_core.lua
+++ grid_core.lua
@@ -184,11 +184,9 @@ end)
 
 -- set uv
 local function setGridUV(offset, layer, i, layer_space)
-    local y = (#layers - i + 1) * layer_space
-
     local matrix = matrices.mat3()
 
-    local size = (1 / offset.y) * math.max(layer.depth or 0, -y) + 1
+    local size = (1 / offset.y) * math.max(layer.depth or 0, 0) + 1
 
     local translate = offset.xz / -grid_size
 
@@ -202,7 +200,7 @@ local function setGridUV(offset, layer, i, layer_space)
 
     layer.model:setUVMatrix(matrix)
 
-    layer.model:setPos(0, math.max(-(layer.depth or 0), y) * 16, 0)
+    layer.model:setPos(0, math.max(-(layer.depth or 0), 0) * 16 + (#layers - i + 1) * layer_space, 0)
 end
 
 -- render grid
@@ -227,7 +225,7 @@ function events.world_render(delta)
     )
 
     
-    local layer_space = math.clamp(distance * 0.0001, 0.001, 0.005)
+    local layer_space = math.clamp(distance * 0.0001, 0.001, 0.02)
 
     for i = 1, #layers do
         setGridUV(offset, layers[i], i, layer_space)
